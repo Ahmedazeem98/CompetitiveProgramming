4c5e7b96347f3b086fbf093f2d323d614de2439f
Ahmedazeem98 <36716361+Ahmedazeem98@users.noreply.github.com>
2018-02-25 23:40:25 +0200

    Create CF400-D2-B.cpp

---
+++ Codeforces/CF400-D2-B.cpp
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <set>
+#include <map>
+#include <numeric>
+#include <bitset>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <deque>
+#include <algorithm>
+#include <iterator>
+#include <utility> 
+#include <stdlib.h>  //atoi
+using namespace std;
+typedef long long ll;
+const  double PI = 3.141592654;
+#define rep(i,x) for(int(i)=0;(i)<(ll)(x);(i)++)
+#define all(s)      (s).begin(),(s).end()
+#define allv(v)      (v).begin(),(v).end()
+#define allr(v)      (v).rbegin(),(v).rend()
+#define pb          push_back
+#define sz          s.size()
+#define ssz          ss.size()
+#define vz          v.size()
+//x = atoi(s.c_str())
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+void fast()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL), cout.tie(NULL);
+}
+int main()
+{
+	fast();
+	int x, y, can = 0;
+	cin >> x >> y;
+	vector<string>v(x);
+	string s;
+	bool test = 0;
+	int c = 0, t = 0;
+	set<int>st;
+	rep(i, x)
+	{
+		cin >> v[i];
+		int dwarf = v[i].find('G'), can = v[i].find('S');
+		if (dwarf > can)
+			test = 1;
+		st.insert(abs(dwarf - can));
+	}
+	if (test)
+		cout << -1;
+	else
+		cout << st.size();
+	cout << endl;
+	return 0;
+}
