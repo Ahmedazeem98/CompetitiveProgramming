6f00b3181176b43bee91fbbda3626c922b367bc6
Ahmedazeem98 <36716361+Ahmedazeem98@users.noreply.github.com>
2018-03-02 03:24:03 +0200

    Create CF236-D2-A.cpp

---
+++ CF236-D2-A.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <set>
+#include <map>
+#include <numeric>
+#include <bitset>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <deque>
+#include <algorithm>
+#include <iterator>
+#include <utility> 
+#include <stdlib.h>  //atoi
+using namespace std;
+typedef long long ll;
+const  double PI = 3.141592654;
+#define rep(i,x) for(int(i)=0;(i)<(ll)(x);(i)++)
+#define all(s)      (s).begin(),(s).end()
+#define allv(v)      (v).begin(),(v).end()
+#define allr(v)      (v).rbegin(),(v).rend()
+#define pb          push_back
+#define sz          s.size()
+#define ssz          ss.size()
+#define vz          v.size()
+//x = atoi(s.c_str())
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+void fast()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL), cout.tie(NULL);
+}
+int main()
+{
+	fast();
+	string s, ss;
+	cin >> s >> ss;
+	if (s.size() > ss.size())
+		cout << -1 << endl;
+	else if (s.size() < ss.size())
+		cout << 1 << endl;
+	else
+	{
+		rep(i, sz)
+		{
+			s[i] = tolower(s[i]), ss[i] = tolower(ss[i]);
+			if (s[i] < ss[i])
+			{
+				cout << -1 << endl;
+				return 0;
+			}
+			else if (s[i]>ss[i])
+			{
+				cout << 1 << endl;
+				return 0;
+			}
+		}
+		cout << 0 << endl;
+	}
+	return 0;
+}
