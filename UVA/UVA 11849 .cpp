6996a1db0a1728baca67d42711742e690005bfca
Ahmedazeem98 <36716361+Ahmedazeem98@users.noreply.github.com>
2018-03-12 22:32:22 +0200

    Create UVA 11849 .cpp

---
+++ UVA/UVA 11849 .cpp
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+using namespace std;
+typedef long long ll;
+const  double PI = 3.141592654;
+#define rep(i,x) for(int(i)=0;(i)<(ll)(x);(i)++)
+#define all(s)      (s).begin(),(s).end()
+#define allv(v)      (v).begin(),(v).end()
+#define allr(v)      (v).rbegin(),(v).rend()
+#define pb          push_back
+#define sz          s.size()
+#define ssz          ss.size()
+#define vz          v.size()
+//x = atoi(s.c_str())
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+void fast()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL), cout.tie(NULL);
+}
+int main()
+{
+	fast();
+	int x, y;
+	while (cin >> x >> y && x != 0 && y != 0)
+	{
+		map<int, int>mp;
+		map<int, int>::iterator it;
+		x += y;
+		while (x--)
+		{
+			cin >> y;
+			mp[y]++;
+		}
+		y = 0;
+		for (it = mp.begin(); it != mp.end(); it++)
+		{
+			if (it->second == 2)
+				y++;
+		}
+		cout << y << endl;
+	}
+	return 0;
+}
