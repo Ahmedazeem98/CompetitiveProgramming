b79d583a4f9d917c0545b910421aaf31c59089e0
Ahmedazeem98 <36716361+Ahmedazeem98@users.noreply.github.com>
2018-04-15 04:46:27 +0200

    Create CF812-D2-C.cpp

---
+++ CF812-D2-C.cpp
@@ -0,0 +1,110 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+using namespace std;
+typedef long long ll;
+const  double PI = 3.141592654;
+#define rep(i,x) for(int(i)=0;(i)<(ll)(x);(i)++)
+#define all(s)      (s).begin(),(s).end()
+#define allv(v)      (v).begin(),(v).end()
+#define allr(v)      (v).rbegin(),(v).rend()
+#define pb          push_back
+#define sz          s.size()
+#define ssz          ss.size()
+#define vz          v.size()
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+int dx[] = { 0, 0, -1, 1, -1, 1, -1, 1 };
+int dy[] = { 1, -1, 0, 0, 1, 1, -1, -1 };
+//x = atoill(s.c_str())
+void fast()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL), cout.tie(NULL);
+}
+int x, s;
+vector<ll>v(100000);
+vector<ll>::iterator it;
+ll mn = LLONG_MAX, ans = 0, sol;
+bool valid(ll m)
+{
+	ll sum = s;
+	int f;
+	ans = 0;
+	vector<ll>mm;
+	for (int i = 0; i < x; i++)
+	{
+		f = i + 1;
+		ll n = (v[i]) + (m * f);
+		mm.pb(n);
+	}
+	sort(allv(mm));
+	rep(i, m)
+		ans += mm[i];
+	if (ans <= s)
+		return 1;
+	else
+		return 0;
+}
+void search( int k)
+{
+	ll l = 1, r = k;
+	while (l <= r)
+	{
+		ll md = (l + r) / 2;
+		if (valid(md))
+		{
+			l = md + 1;
+			if (md > sol)
+			mn = ans, sol = md;
+		}
+		else
+			r = md - 1;
+	}
+}
+int main()
+{
+	fast();
+	cin >> x >> s;
+	rep(i, x)
+		cin >> v[i];
+	search(x);
+	if (mn == LLONG_MAX)
+		mn = 0;
+	cout << sol << " " << mn << endl;
+	return 0;
+}
