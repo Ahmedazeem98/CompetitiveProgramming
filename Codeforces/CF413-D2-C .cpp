969cd94bf3c29c6362bddade6de4c494c1b0d20a
Ahmedazeem98 <36716361+Ahmedazeem98@users.noreply.github.com>
2018-06-23 00:58:40 +0200

    Create CF413-D2-C .cpp

---
+++ Codeforces/CF413-D2-C .cpp
@@ -0,0 +1,91 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+typedef long long ll;
+const  double PI = 3.141592654;
+#define rep(i,x) for(int(i)=0;(i)<(ll)(x);(i)++)
+#define all(s)      (s).begin(),(s).end()
+#define allv(v)      (v).begin(),(v).end()
+#define allr(v)      (v).rbegin(),(v).rend()
+#define pb          push_back
+#define sz          s.size()
+#define ssz          ss.size()
+#define vz          v.size()
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+#define rep(i,x) for(int(i)=0;(i)<(ll)(x);(i)++)
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+void fast()
+{
+#ifndef ONLINE_JUDGE
+
+	/*
+
+	**    ** ** **    *********   ** ** **
+	**    **          **     **   **
+	**    **          *********   **
+	**    **          **          **
+	**    ** ** **    **          ** ** **
+
+	*/
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL), cout.tie(NULL);
+}
+int main()
+{
+	fast();
+	int x, y, a;
+	cin >> x >> y;
+	ll sum = 0;
+	vector<int>v(x);
+	rep(i, x)
+		cin >> v[i];
+	priority_queue<ll>pr;
+	rep(i, y)
+		cin >> a, pr.push(v[a - 1]), v[a - 1] = 0;
+	rep(i, x)
+		sum += v[i];
+	while (!pr.empty())
+	{
+		sum += max(pr.top(), sum);
+		pr.pop();
+	}
+	cout << sum;
+	return 0;
+}
