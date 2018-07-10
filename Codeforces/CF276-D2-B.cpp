5537085cca7e0c92123cd5c11c5b7793b1f2145a
Ahmedazeem98 <36716361+Ahmedazeem98@users.noreply.github.com>
2018-07-10 15:00:24 +0200

    Create CF276-D2-B.cpp

---
+++ Codeforces/CF276-D2-B.cpp
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+using namespace std;
+typedef long long ll;
+const  double PI = 3.141592654;
+#define rep(i,x) for(int(i)=0;(i)<(ll)(x);(i)++)
+#define all(s)      (s).begin(),(s).end()
+#define allv(v)      (v).begin(),(v).end()
+#define allvr(v)      (v).rbegin(),(v).rend()
+#define pb          push_back
+#define sz          s.size()
+#define ssz          ss.size()
+#define vz          v.size()
+void fast()
+{
+#ifndef ONLINE_JUDGE
+
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL), cout.tie(NULL);
+}
+vector<int>ans;
+int main()
+{
+	fast();
+	string s;
+	cin >> s;
+	map<char, int>mp;
+	rep(i, sz)
+		mp[s[i]]++;
+	int c = 0;
+	for (auto e : mp)
+	{
+		if (e.second & 1)
+			c++;
+	}
+	if (c & 1 || c == 0)
+		cout << "First";
+	else
+		cout << "Second";
+	return 0;
+}
