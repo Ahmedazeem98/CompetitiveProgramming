7297c49c6c1e5a61999545e14d3e485993537a20
Ahmedazeem98 <36716361+Ahmedazeem98@users.noreply.github.com>
2018-11-21 14:58:56 +0200

    Update UVA  10611 .cpp

--- UVA/UVA  10611 .cpp
+++ UVA/UVA  10611 .cpp
@@ -23,83 +23,96 @@
 #include <queue>
 #include <set>
 #include <sstream>
+#include <unordered_map>
 #include <stack>
 #include <stdexcept>
 #include <streambuf>
 #include <string>
 #include <typeinfo>
+#include<cstdlib>
 #include <utility>
 #include <valarray>
 #include <vector>
 using namespace std;
-typedef long long ll;
-const  double PI = 3.141592654;
-#define rep(i,x) for(int(i)=0;(i)<(ll)(x);(i)++)
-#define all(s)      (s).begin(),(s).end()
-#define allv(v)      (v).begin(),(v).end()
-#define allr(v)      (v).rbegin(),(v).rend()
-#define pb          push_back
-#define sz          s.size()
-#define ssz          ss.size()
-#define vz          v.size()
-ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
-ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
-//x = atoi(s.c_str())
-void fast()
+void file()
 {
 #ifndef ONLINE_JUDGE
+
 	freopen("in.txt", "r", stdin);
 	freopen("out.txt", "w", stdout);
 #endif
 	ios_base::sync_with_stdio(0);
 	cin.tie(NULL), cout.tie(NULL);
 }
+typedef long long ll;
+#define endl "\n"
+#define rep(i,x) for(int(i)=0;(i)<(ll)(x);(i)++)
+#define all(s)      (s).begin(),(s).end()
+#define allv(v)      (v).begin(),(v).end()
+#define allvr(v)      (v).rbegin(),(v).rend()
+#define pb          push_back
+#define sz          s.size()
+#define ssz          ss.size()
+#define vz          v.size()
+pair<ll, ll> search(vector<ll>v, ll x)
+{
+	ll low = 0, high = vz - 1, a = -1 * 1e17, b = 1e17;
+	while (low <= high)
+	{
+		int md = (low + high) / 2;
+		int val = v[md];
+		if (v[md] > x)
+		{
+			b = min(b, v[md]);
+			high = md - 1;
+		}
+		else if (v[md] < x)
+		{
+			a = max(a, v[md]);
+			low = md+ 1;
+		}
+		else
+		{
+			if (md)
+				a = max(a, v[md - 1]);
+			if (md + 1 < vz)
+				b = min(b, v[md + 1]);
+			break;
+		}
+	}
+	return{ a, b };
+}
 int main()
 {
-	fast();
-	ll x, t;
+	file();
+	int x, y;
 	while (cin >> x)
 	{
-		ll a, mx = -1;
-		set<ll>st;
+		vector<ll>v;
+		map<ll, int>mp;
+		int idx = 0;
 		rep(i, x)
 		{
-			ll m;
-			cin >> m;
-			st.insert(m);
-			mx = max(mx, m);
-		}
-		vector<ll>v(st.begin(),st.end());
-		vector<ll>::iterator it;
-		cin >> t;
-		rep(i, t)
-		{
-			bool find = 0;
+			int a;
 			cin >> a;
-			it = lower_bound(all(v), a);
-			int idx = it - v.begin();
-			if (binary_search(allv(v),a))
-				find = 1;
-			idx--;
-			if (idx >= 0 && idx < vz)
-				cout << v[idx] << " ";
-			else
-			{
-				if (a>x)
-					cout << v[x - 1] << " ";
-				else
-					cout << "X" << " ";
-			}
-			if (find)
-				idx += 2;
-			else
-				idx++;
-			if (idx >= 0 && idx < vz)
-				cout << v[idx] << endl;
-			else
-				cout << "X" << endl;
+			if (mp[a] == 0)
+				v.push_back(a), mp[a] = 1;
 		}
-
+		cin >> y;
+		while (y--)
+		{
+			ll n;
+			cin >> n;
+			pair<ll, ll>ans = search(v, n);
+			if (ans.first != ll(-1*1e17))
+				cout << ans.first << " ";
+			else
+				cout << 'X' << " ";
+			if (ans.second != ll(1e17))
+				cout << ans.second << "\n";
+			else
+				cout << 'X' << "\n";
+		}
 	}
 	return 0;
 }
